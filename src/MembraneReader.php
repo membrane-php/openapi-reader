3b08bf6f1e9043f326003050ee0d4b9cc7bc4a57
John Charman <john.charman@imhotek.net>
2024-01-19 15:13:42 +0000

    Add Membrane Reader

---
+++ src/MembraneReader.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Membrane\OpenAPIReader;
+
+use cebe\{openapi as Cebe, openapi\exceptions as CebeException, openapi\spec as CebeSpec};
+use Closure;
+use Membrane\OpenAPIReader\Exception\{CannotRead, CannotSupport, InvalidOpenAPI};
+use Membrane\OpenAPIReader\Factory\V30\FromCebe;
+use Membrane\OpenAPIReader\ValueObject\Valid\V30\OpenAPI;
+use Symfony\Component\Yaml\Exception\ParseException;
+use TypeError;
+
+final class MembraneReader
+{
+    /** @param OpenAPIVersion[] $supportedVersions */
+    public function __construct(
+        private readonly array $supportedVersions,
+    ) {
+        if (empty($this->supportedVersions)) {
+            throw CannotSupport::noSupportedVersions();
+        }
+
+        if ($this->supportedVersions !== [OpenAPIVersion::Version_3_0]) {
+            throw CannotSupport::membraneReaderOnlySupportsv30();
+        }
+    }
+
+    public function readFromAbsoluteFilePath(string $absoluteFilePath, ?FileFormat $fileFormat = null): OpenAPI
+    {
+        file_exists($absoluteFilePath) ?: throw CannotRead::fileNotFound($absoluteFilePath);
+
+        $fileFormat ??= FileFormat::fromFileExtension(pathinfo($absoluteFilePath, PATHINFO_EXTENSION));
+
+        try {
+            $openAPI = $this->getCebeObject(match ($fileFormat) {
+                FileFormat::Json => fn() => Cebe\Reader::readFromJsonFile($absoluteFilePath),
+                FileFormat::Yaml => fn() => Cebe\Reader::readFromYamlFile($absoluteFilePath),
+                default => throw CannotRead::unrecognizedFileFormat($absoluteFilePath)
+            });
+        } catch (CebeException\UnresolvableReferenceException $e) {
+            throw CannotRead::unresolvedReference($e);
+        }
+
+        return $this->getValidatedObject($openAPI);
+    }
+
+    public function readFromString(string $openAPI, FileFormat $fileFormat): OpenAPI
+    {
+        if (preg_match('#\s*[\'\"]?\$ref[\'\"]?\s*:\s*[\'\"]?[^\s\'\"\#]#', $openAPI)) {
+            throw CannotRead::cannotResolveExternalReferencesFromString();
+        }
+
+        $openAPI = $this->getCebeObject(match ($fileFormat) {
+            FileFormat::Json => fn() => Cebe\Reader::readFromJson($openAPI),
+            FileFormat::Yaml => fn() => Cebe\Reader::readFromYaml($openAPI),
+        });
+
+        try {
+            $openAPI->resolveReferences(new Cebe\ReferenceContext($openAPI, '/tmp'));
+        } catch (CebeException\UnresolvableReferenceException $e) {
+            throw CannotRead::unresolvedReference($e);
+        }
+
+        return $this->getValidatedObject($openAPI);
+    }
+
+    /** @param Closure():CebeSpec\OpenApi $readOpenAPI */
+    private function getCebeObject(Closure $readOpenAPI): CebeSpec\OpenApi
+    {
+        try {
+            return $readOpenAPI();
+        } catch (TypeError | CebeException\TypeErrorException | ParseException $e) {
+            throw CannotRead::invalidFormatting($e);
+        }
+    }
+
+    private function getValidatedObject(CebeSpec\OpenApi $openAPI): OpenAPI
+    {
+        $this->isVersionSupported($openAPI->openapi) ?: throw CannotSupport::unsupportedVersion($openAPI->openapi);
+
+        /** todo create 3.1 validated objects */
+        if (OpenAPIVersion::fromString($openAPI->openapi) !== OpenAPIVersion::Version_3_0) {
+            throw CannotSupport::membraneReaderOnlySupportsv30();
+        }
+
+        $validatedObject = FromCebe::createOpenAPI($openAPI);
+
+        $openAPI->validate() ?: throw InvalidOpenAPI::failedCebeValidation(...$openAPI->getErrors());
+
+        return $validatedObject;
+    }
+
+    private function isVersionSupported(string $version): bool
+    {
+        return in_array(OpenAPIVersion::fromString($version), $this->supportedVersions, true);
+    }
+}
--- tests/Factory/V30/FromCebeTest.php
+++ tests/Factory/V30/FromCebeTest.php
@@ -8,7 +8,7 @@ use cebe\openapi\spec as Cebe;
 use Generator;
 use Membrane\OpenAPIReader\Factory\V30\FromCebe;
 use Membrane\OpenAPIReader\Method;
-use Membrane\OpenAPIReader\Tests\Fixtures\Helper\PartialHelper;
+use Membrane\OpenAPIReader\Tests\Fixtures\Helper\OpenAPIProvider;
 use Membrane\OpenAPIReader\ValueObject\Partial;
 use Membrane\OpenAPIReader\ValueObject\Valid\Identifier;
 use Membrane\OpenAPIReader\ValueObject\Valid\V30\MediaType;
@@ -60,185 +60,13 @@ class FromCebeTest extends TestCase
     public static function provideCebeOpenAPIObjects(): Generator
     {
         yield 'minimal OpenAPI' => [
-            new OpenAPI(PartialHelper::createOpenAPI(
-                openapi: '3.0.0',
-                title: 'Test API',
-                version: '1.0.1',
-                paths: []
-            )),
-            new Cebe\OpenApi([
-                'openapi' => '3.0.0',
-                'info' => ['title' => 'Test API', 'version' => '1.0.1'],
-                'paths' => [],
-            ])
+            OpenAPIProvider::minimalV30MembraneObject(),
+            OpenAPIProvider::minimalV30CebeObject(),
         ];
 
         yield 'detailed OpenAPI' => [
-            new OpenAPI(PartialHelper::createOpenAPI(
-                openapi: '3.0.0',
-                title: 'Test API',
-                version: '1.0.1',
-                servers: [
-                    PartialHelper::createServer(url: 'https://server.net'),
-                ],
-                paths: [
-                    PartialHelper::createPathItem(
-                        path: '/first',
-                        parameters: [
-                            PartialHelper::createParameter(
-                                name: 'limit',
-                                in: 'query',
-                                required: false,
-                                schema: PartialHelper::createSchema(
-                                    type: 'integer'
-                                )
-                            ),
-                        ],
-                        get: PartialHelper::createOperation(
-                            operationId: 'first-get',
-                            parameters: [
-                                PartialHelper::createParameter(
-                                    name: 'pet',
-                                    in: 'header',
-                                    required: true,
-                                    schema: null,
-                                    content: [
-                                        PartialHelper::createMediaType(
-                                            mediaType: 'application/json',
-                                            schema: PartialHelper::createSchema(
-                                                allOf: [
-                                                    PartialHelper::createSchema(
-                                                        type: 'integer'
-                                                    ),
-                                                    PartialHelper::createSchema(
-                                                        type: 'number'
-                                                    )
-                                                ]
-                                            )
-                                        )
-                                    ]
-                                )
-                            ]
-                        )
-                    ),
-                    PartialHelper::createPathItem(
-                        path: '/second',
-                        servers: [
-                            PartialHelper::createServer(
-                                url: 'https://second-server.co.uk'
-                            ),
-                        ],
-                        parameters: [
-                            PartialHelper::createParameter(
-                                name: 'limit',
-                                in: 'query',
-                                required: false,
-                                schema: PartialHelper::createSchema(
-                                    type: 'integer'
-                                )
-                            ),
-                        ],
-                        get: PartialHelper::createOperation(
-                            operationId: 'second-get',
-                            parameters: [
-                                PartialHelper::createParameter(
-                                    name: 'pet',
-                                    in: 'header',
-                                    required: true,
-                                    schema: null,
-                                    content: [
-                                        PartialHelper::createMediaType(
-                                            mediaType: 'application/json',
-                                            schema: PartialHelper::createSchema(
-                                                allOf: [
-                                                    PartialHelper::createSchema(
-                                                        type: 'integer'
-                                                    ),
-                                                    PartialHelper::createSchema(
-                                                        type: 'number'
-                                                    )
-                                                ]
-                                            )
-                                        )
-                                    ]
-                                )
-                            ]
-                        )
-                    )
-                ]
-            )),
-            new Cebe\OpenApi([
-                'openapi' => '3.0.0',
-                'info' => ['title' => 'Test API', 'version' => '1.0.1'],
-                'servers' => [
-                    ['url' => 'https://server.net']
-                ],
-                'paths' => [
-                    '/first' => [
-                        'parameters' => [
-                            [
-                                'name' => 'limit',
-                                'in' => 'query',
-                                'required' => false,
-                                'schema' => ['type' => 'integer']
-                            ]
-                        ],
-                        'get' => [
-                            'operationId' => 'first-get',
-                            'parameters' => [
-                                [
-                                    'name' => 'pet',
-                                    'in' => 'header',
-                                    'required' => true,
-                                    'content' => [
-                                        'application/json' => [
-                                            'schema' => [
-                                                'allOf' => [
-                                                    ['type' => 'integer'],
-                                                    ['type' => 'number']
-                                                ]
-                                            ]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ],
-                    '/second' => [
-                        'servers' => [
-                            ['url' => 'https://second-server.co.uk']
-                        ],
-                        'parameters' => [
-                            [
-                                'name' => 'limit',
-                                'in' => 'query',
-                                'required' => false,
-                                'schema' => ['type' => 'integer']
-                            ]
-                        ],
-                        'get' => [
-                            'operationId' => 'second-get',
-                            'parameters' => [
-                                [
-                                    'name' => 'pet',
-                                    'in' => 'header',
-                                    'required' => true,
-                                    'content' => [
-                                        'application/json' => [
-                                            'schema' => [
-                                                'allOf' => [
-                                                    ['type' => 'integer'],
-                                                    ['type' => 'number']
-                                                ]
-                                            ]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ],
-            ])
+            OpenAPIProvider::detailedV30MembraneObject(),
+            OpenAPIProvider::detailedV30CebeObject(),
         ];
     }
 }
